727ee715e9b9a8932d717f06dc99200f6e99b7f9
jlrussell1990 <jlrussell1990@gmail.com>
2023-08-28 10:33:28 -0500

    ssl true

--- server.js
+++ server.js
@@ -12,11 +12,8 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host: 'postgresql-dimensional-48288',
-    port: 5432,
-    user: '',
-    password: '',
-    database: 'smart-brain'
+    host: process.env.DATABASE_URL,
+    ssl: true,
   }
 });
 
